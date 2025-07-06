7738be98224ca98b7d489ebed7a3677d6132a206
Vaishnavig3 <93038768+Vaishnavig3@users.noreply.github.com>
2025-07-06 10:52:02 +0530

    my changes

--- gcp_pyspark_schedule.sh
+++ gcp_pyspark_schedule.sh
@@ -1,6 +1,7 @@
-gcloud dataproc jobs submit pyspark --cluster=cluster-iz-dplr --region=us-central1 --properties="spark.driver.memory=2g","spark.executor.memory=2g","spark.executor.instances=4","spark.executor.cores=2","spark.submit.deployMode=client","spark.sql.shuffle.partitions=10","spark.shuffle.spill.compress=true" /home/mohamedirfan_mohideen/project/Usecase4_GcpGcsReadWritehive_cloud.py
+#!/bin/bash
+gcloud dataproc jobs submit pyspark --cluster=cluster-vaish-u4 --region=us-central1 --properties="spark.driver.memory=2g","spark.executor.memory=2g","spark.executor.instances=4","spark.executor.cores=2","spark.submit.deployMode=client","spark.sql.shuffle.partitions=10","spark.shuffle.spill.compress=true" /home/vaishnavig3/project/Usecase4_GcpGcsReadWritehive_cloud.py
 #Alternative for spark-submit inside the dataproc cluster
-#spark-submit --master yarn --mode client --num-executors 4….
+#spark-submit --master yarn --mode client….
 if [ $? -ne 0 ]
 then
 echo "`date` error occured in the Pyspark job" > /tmp/gcp_pyspark_schedule.log
